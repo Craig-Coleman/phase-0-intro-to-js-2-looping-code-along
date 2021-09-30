0eb0d3d493966a43509ffe9d8d8c7e0693cbbe81
Craig <craig.coleman.cc@gmail.com>
2021-09-29 20:54:00 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+
+const names = ["Conor", "JL", "Alicia"];
+
+const event1 = "birthday";
+
+function writeCards(names, event1) {
+    
+    const namesCopy = [...names];
+
+    for (let i = 0; i < namesCopy.length; i++) {
+        console.log(`Hello ${namesCopy[i]}, thank you so much for the ${event1}, gift!`)
+    }
+
+    return namesCopy;
+}
+
+writeCards(names, event1);
+
+
+
+let number = 10
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
+
+countDown(number);
